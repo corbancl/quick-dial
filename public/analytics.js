859286ca238e380e164545ef2ccaa7096cc11439
corban <68857@126.com>
2026-06-11 09:41:09 +0800

    fix: replace baidu analytics script injection with CSP-safe image pixel tracking

--- public/analytics.js
+++ public/analytics.js
@@ -1,8 +1,7 @@
-// 百度统计 - 通过外部 JS 文件加载以避免 CSP 限制
-var _hmt = _hmt || [];
+// 百度统计 - 图片像素追踪（兼容 Manifest V3 CSP，不加载外部脚本）
 (function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?f76a8c6d82f80426895a923be73188cb";
-  var s = document.getElementsByTagName("script")[0];
-  s.parentNode.insertBefore(hm, s);
+  var si = 'f76a8c6d82f80426895a923be73188cb';
+  var img = new Image();
+  var r = Math.floor(Math.random() * 2147483647);
+  img.src = 'https://hm.baidu.com/hm.gif?si=' + si + '&et=0&ep=0&nv=0&st=4&v=pixel-1.0&rnd=' + r + '&su=' + encodeURIComponent(document.referrer || '');
 })();
